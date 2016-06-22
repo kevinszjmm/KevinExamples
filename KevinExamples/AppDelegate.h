d9ced63226bbf5f5f59da4247966c2da7dd995f8
zhejunshen <zhejun.shen@ymm56.com>
2016-06-22 11:34:45 +0800

    add KevinExamplees

---
+++ KevinExamples/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KevinExamples
+//
+//  Created by zhejunshen on 16/6/22.
+//  Copyright © 2016年 zhejunshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KevinExamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KevinExamples
+//
+//  Created by zhejunshen on 16/6/22.
+//  Copyright © 2016年 zhejunshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
